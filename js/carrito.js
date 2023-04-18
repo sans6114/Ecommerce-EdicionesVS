95491e18aacd8410e4c1fb958c7605e10ea81b6a
Santiago Sosa <santisosalp@gmail.com>
2023-04-18 19:39:47 -0300

    se agregaron cambios en el JS de carrito

---
+++ js/carrito.js
@@ -0,0 +1,51 @@
+const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-compra");
+
+
+if (productosEnCarrito){
+
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+
+    contenedorCarritoProductos.innerHTML = "";
+
+    productosEnCarrito.forEach(productosArray => {
+        
+        
+        const div = document.createElement("div");
+        div.classList.add("carrito-producto");
+        div.innerHTML = `<img class="img-carrito" src="${productosArray.imagen}" alt="${productosArray.titulo}">
+        <div class="carrito-producto-titulo">
+            <small>título</small>
+            <h3>${productosArray.titulo}</h3>
+        </div>
+        <div class="carrito-producto-cantidad">
+            <small>Cantidad</small>
+            <p>${productosArray.cantidad}</p>
+        </div>
+        <div class="carrito-producto-precio">
+            <small>Precio</small>
+            <p>${productosArray.precio}</p>
+        </div>
+        <div class="carrito-producto-subtotal">
+            <small>Subtotal</small>
+            <p>${productosArray.precio * productosArray.cantidad}</p>
+        </div>
+        <button id="${productosArray.id}" class="carrito-producto-eliminar">
+            <i class="bi bi-trash-fill"></i>
+        </button>
+        `;
+
+        contenedorCarritoProductos.append("div");
+    })
+
+
+}else{
+
+}
--- main.js
+++ main.js
@@ -2,41 +2,62 @@
 
 const productosArray = [
     {
-        id:"libro-01",
-        titulo:"Mujer Maravilla (Natalia Spetale)",
-        imagen:"./img/mujermaravilla_tapa.jpg",
+        id: "libro-01",
+        titulo: "Mujer Maravilla (Natalia Spetale)",
+        imagen: "./img/mujermaravilla_tapa.jpg",
+        categoria: {
+            nombre: "x1",
+            id: "x1"
+        },
         precio: "1500"
     },
     {
-        id:"libro-01",
-        titulo:"¿Qué tienes en tu Mano? (Mariana Maillet)",
-        imagen:"./img/tapa_quetienesentumano_marianamaillet.jpg",
-        precio: "1500"   
-    },
-    {
-        id:"libro-01",
-        titulo:"Antídotos contra la religión (Juan Pablo Sosa)",
-        imagen:"./img/antidotoscontralareligion_tapa.jpg",
+        id: "libro-01",
+        titulo: "¿Qué tienes en tu Mano? (Mariana Maillet)",
+        imagen: "./img/tapa_quetienesentumano_marianamaillet.jpg",
+        categoria: {
+            nombre: "x2",
+            id: "x2"
+        },
         precio: "1500"
     },
     {
-        id:"libro-01",
-        titulo:"Hijos de la Promesa (Ezequiel Rossini)",
-        imagen:"./img/HijosDeLaPromesa-EzequielRossini_160p-14x21-1-555x832-1.jpg",
+        id: "libro-01",
+        titulo: "Antídotos contra la religión (Juan Pablo Sosa)",
+        imagen: "./img/antidotoscontralareligion_tapa.jpg",
+        categoria: {
+            nombre: "x3",
+            id: "x4"
+        },
+        precio: "1500"
+    },
+    {
+        id: "libro-01",
+        titulo: "Hijos de la Promesa (Ezequiel Rossini)",
+        imagen: "./img/HijosDeLaPromesa-EzequielRossini_160p-14x21-1-555x832-1.jpg",
+        categoria: {
+            nombre: "x3",
+            id: "x3"
+        },
         precio: "1500"
     }
 ]
 
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
-let botonesAgregar = document.querySelectorAll(".producto-precio")
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-precio");
+const numerito = document.querySelector("#numerito");
 
-function cargarProductos() {
+function cargarProductos(productosElegidos) {
 
-    productosArray.forEach(producto => { 
-    const div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
     <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
                     <div class="producto-detalles">
                         <h3 class="producto-titulo">
@@ -49,15 +70,73 @@ function cargarProductos() {
                     </div>
     `;
 
-    contenedorProductos.append(div);
-console.log(botonesAgregar);
+        contenedorProductos.append(div);
     })
 
-    actualizarBotonesAgregar()
+    actualizarBotonesAgregar();
+    
 }
 
-cargarProductos();
+cargarProductos(productosArray);
 
-function actualizarBotonesAgregar(){
-    botonesAgregar = document.querySelectorAll(".producto-precio")
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productosArray.find(productosArray => productosArray.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+            const productosBoton = productosArray.filter(productosArray => productosArray.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productosArray);
+        }
+    });
+});
+
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-precio");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+const productosEnCarritoLS = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+if(productosEnCarritoLS) {
+    productosEnCarrito = productosEnCarritoLS;
+} else {
+    productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productosArray.find(producto => producto.id === idBoton);
+
+
+    if(productosEnCarrito.some(productosArray => productosArray.id === idBoton)){
+        const index = productosEnCarrito.findIndex(productosArray => productosArray.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    }else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+};
+
+function actualizarNumerito(){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito
 }
