615e45aabed3ca00384904fb63786bff148d706a
Santiago Sosa <santisosalp@gmail.com>
2023-04-19 19:26:17 -0300

    se modificaron los ruteos

--- js/carrito.js
+++ js/carrito.js
@@ -3,10 +3,10 @@ const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-const contenedorCarritoComprado = document.querySelector("#carrito-compra");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
 
 
-if (productosEnCarrito){
+if (productosEnCarritoLS){
 
     contenedorCarritoVacio.classList.add("disabled");
     contenedorCarritoProductos.classList.remove("disabled");
